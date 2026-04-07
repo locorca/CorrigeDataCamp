fdaef61b940259c1faccece5dc965326715964be
locorca <locorca@alumni.uv.es>
2026-04-07 20:28:32 +0200

    Buscar ficheros de puntuaciones y ficheros con justificantes

--- script_correccion.R
+++ script_correccion.R
@@ -3,3 +3,13 @@ library(readxl)
 library(writexl)
 
 directorio_entregas <- "entregas_temp"
+
+ficheros_puntuaciones <- list.files(path = directorio_entregas, 
+                              pattern = "(?i)puntos.*\\.txt$", 
+                              recursive = TRUE, 
+                              full.names = TRUE)
+
+ficheros_justificante <- list.files(path = directorio_entregas, 
+                            pattern = "(?i)justificante|certif.*\\.(pdf|png|jpg)$", 
+                            recursive = TRUE, 
+                            full.names = TRUE)
